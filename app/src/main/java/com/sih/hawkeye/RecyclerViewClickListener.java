a3ec524a22f62b65f58bc9c6ee0ff6ee360ab78a
JithinJude <jithin.jude68@gmail.com>
2020-02-04 10:16:42 +0530

    intial commit

---
+++ app/src/main/java/com/sih/hawkeye/RecyclerViewClickListener.java
@@ -0,0 +1,6 @@
+package com.sih.hawkeye;
+
+public interface RecyclerViewClickListener {
+    //ABSTRACT METHOD
+    void onItemClick(int pos);
+}
---
+++ app/src/main/java/com/sih/hawkeye/RecyclerviewDataModel.java
@@ -0,0 +1,13 @@
+package com.sih.hawkeye;
+
+public class RecyclerviewDataModel {
+
+    public int imageId;
+    public String txt;
+
+    RecyclerviewDataModel( int imageId, String text) {
+
+        this.imageId = imageId;
+        this.txt = text;
+    }
+}
