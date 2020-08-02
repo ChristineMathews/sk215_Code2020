ce0ccfb8cd6429a19c5a80f5c6b31216422f88f6
JithinJude <jithin.jude68@gmail.com>
2020-08-02 10:25:34 +0530

    implemented Image crop lib for OCR

--- app/src/main/java/com/sih/hawkeye/ocr/OcrActivity.java
+++ app/src/main/java/com/sih/hawkeye/ocr/OcrActivity.java
@@ -1,35 +1,58 @@
 package com.sih.hawkeye.ocr;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Build;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.FileProvider;
+
 import android.os.Bundle;
-import android.util.Base64;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.text.format.DateFormat;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.mlkit.vision.common.InputImage;
 import com.google.mlkit.vision.text.Text;
 import com.google.mlkit.vision.text.TextRecognition;
 import com.google.mlkit.vision.text.TextRecognizer;
 import com.sih.hawkeye.R;
+import com.soundcloud.android.crop.Crop;
 
-import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
 import java.util.Objects;
 
 public class OcrActivity extends AppCompatActivity {
 
+    private static final int REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS = 101;
+    String CAMERA_PERMISSION = android.Manifest.permission.CAMERA;
+
+    String READ_EXTERNAL_STORAGE_PERMISSION = android.Manifest.permission.READ_EXTERNAL_STORAGE;
+    String WRITE_EXTERNAL_STORAGE_PERMISSION = android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+
     private static final int CAMERA_REQUEST = 1;
 
     Bitmap ocrPhoto;
+    Uri imageFileUri;
+    Uri croppedImageFileUri;
+
+    FloatingActionButton fab;
 
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
@@ -40,7 +63,20 @@ public class OcrActivity extends AppCompatActivity {
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
         setTitle(getString(R.string.ocr_activity_title));
 
-        takePhoto();
+        fab = findViewById(R.id.ocrFab);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    checkMultiplePermissions(REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS, OcrActivity.this);
+                } else {
+                    // Open your camera here.
+                    takePhoto();
+                }
+            }
+        });
+
     }
 
     @Override
@@ -60,13 +96,71 @@ public class OcrActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
-            ocrPhoto = (Bitmap) data.getExtras().get("data");
-            runTextRecognition(ocrPhoto);
+//            ocrPhoto = (Bitmap) data.getExtras().get("data");
+
+            Crop.of(imageFileUri, croppedImageFileUri).start(this);
+
+//            runTextRecognition(ocrPhoto);
+        }
+    }
+
+    //check for camera and storage access permissions
+    @TargetApi(Build.VERSION_CODES.M)
+    private void checkMultiplePermissions(int permissionCode, Context context) {
+
+        String[] PERMISSIONS = {CAMERA_PERMISSION, READ_EXTERNAL_STORAGE_PERMISSION, WRITE_EXTERNAL_STORAGE_PERMISSION};
+        if (!hasPermissions(context, PERMISSIONS)) {
+            ActivityCompat.requestPermissions((Activity) context, PERMISSIONS, permissionCode);
+        } else {
+            // Open your camera here.
+            takePhoto();
+        }
+    }
+
+    private boolean hasPermissions(Context context, String... permissions) {
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && context != null && permissions != null) {
+            for (String permission : permissions) {
+                if (ActivityCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED && grantResults[2] == PackageManager.PERMISSION_GRANTED) {
+                    // Call your camera here.
+                    takePhoto();
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
 
     public void takePhoto() {
-        Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        final String dir =  Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)+ "/Garuda/";
+        File garudaDir = new File(dir);
+        garudaDir.mkdirs();
+
+        String file = dir + "ocr_" + DateFormat.format("yyyy-MM-dd_hhmmss", new Date()).toString()+".jpg";
+
+        File mFile = new File(file);
+        try {
+            mFile.createNewFile();
+        } catch (IOException e) {}
+
+        imageFileUri = FileProvider.getUriForFile(this, this.getApplicationContext().getPackageName() + ".provider", mFile);
+        croppedImageFileUri = FileProvider.getUriForFile(this, this.getApplicationContext().getPackageName() + ".provider", mFile);
+
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        cameraIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageFileUri);
         startActivityForResult(cameraIntent, CAMERA_REQUEST);
     }
 
