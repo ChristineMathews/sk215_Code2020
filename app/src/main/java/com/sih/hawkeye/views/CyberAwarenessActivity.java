f7083166a02fd0193332ae90dadde8f1a6074960
JithinJude <jithin.jude68@gmail.com>
2020-08-02 23:43:48 +0530

    FIX: View back for UX.

--- app/src/main/java/com/sih/hawkeye/views/CyberAwarenessActivity.java
+++ app/src/main/java/com/sih/hawkeye/views/CyberAwarenessActivity.java
@@ -18,6 +18,7 @@ import java.util.Objects;
 public class CyberAwarenessActivity extends AppCompatActivity {
 
     ProgressDialog progressDialog;
+    WebView browser;
 
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
@@ -35,7 +36,7 @@ public class CyberAwarenessActivity extends AppCompatActivity {
         progressDialog.setCancelable(false);
         progressDialog.show();
 
-        final WebView browser = (WebView) findViewById(R.id.connect_volunteers_online);
+        browser = (WebView) findViewById(R.id.connect_volunteers_online);
 
         browser.getSettings().setDomStorageEnabled(true);
         browser.getSettings().setJavaScriptEnabled(true);
@@ -77,5 +78,14 @@ public class CyberAwarenessActivity extends AppCompatActivity {
             onBackPressed();
         }
         return true;
+    }
+
+    @Override
+    public void onBackPressed(){
+        if (browser.canGoBack()) {
+            browser.goBack();
+        } else {
+            finish();
+        }
     }
 }
