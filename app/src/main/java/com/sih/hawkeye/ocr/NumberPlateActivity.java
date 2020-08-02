7a8161179d7b4242040a947fc1c1f42e36f53578
JithinJude <jithin.jude68@gmail.com>
2020-08-02 11:50:26 +0530

    vehicle regNo to RealTime DB

--- app/src/main/java/com/sih/hawkeye/ocr/NumberPlateActivity.java
+++ app/src/main/java/com/sih/hawkeye/ocr/NumberPlateActivity.java
@@ -4,10 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.sih.hawkeye.Issue;
 import com.sih.hawkeye.R;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class NumberPlateActivity extends AppCompatActivity {
 
@@ -16,19 +26,46 @@ public class NumberPlateActivity extends AppCompatActivity {
 
     EditText editTextVehicleRegNo;
     ImageView imageViewVehicleImage;
+    Button buttonReport;
+
+    DatabaseReference databaseVehicle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_number_plate);
 
+        databaseVehicle = FirebaseDatabase.getInstance().getReference("vehicles");
+
         vehicleRegNo = getIntent().getStringExtra("KEY_VEHICLE_REG_NO");
         vehicleImageUrl = getIntent().getStringExtra("KEY_VEHICLE_IMAGE_URL");
 
         editTextVehicleRegNo = findViewById(R.id.edVehicleRegNo);
         imageViewVehicleImage = findViewById(R.id.ivVehicleImg);
+        buttonReport = findViewById(R.id.btnReportVehicle);
 
         editTextVehicleRegNo.setText(vehicleRegNo);
         imageViewVehicleImage.setImageURI(Uri.parse(vehicleImageUrl));
+
+        buttonReport.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                vehicleRegNo = editTextVehicleRegNo.getText().toString();
+                reportVehicle();
+            }
+        });
+    }
+
+    public void reportVehicle(){
+
+        String id = databaseVehicle.push().getKey();
+        String date = new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault()).format(new Date());
+
+        Vehicle vehicle = new Vehicle(id, vehicleRegNo, "dummyImgUrl", date);
+
+        databaseVehicle.child(id).setValue(vehicle);
+
+        Toast.makeText(this,"Vehicle reported",Toast.LENGTH_LONG).show();
+        finish();
     }
 }
---
+++ app/src/main/java/com/sih/hawkeye/ocr/Vehicle.java
@@ -0,0 +1,53 @@
+package com.sih.hawkeye.ocr;
+
+/**
+ * Created by JithinJude on 02,August,2020
+ */
+class Vehicle {
+    String vehicleReportId;
+    String vehicleRegNo;
+    String vehicleImageUrl;
+    String vehicleReportDate;
+
+    public Vehicle(String vehicleReportId, String vehicleRegNo, String vehicleImageUrl,
+                 String vehicleReportDate){
+        this.vehicleReportId = vehicleReportId;
+        this.vehicleRegNo = vehicleRegNo;
+        this.vehicleImageUrl = vehicleImageUrl;
+        this.vehicleReportDate = vehicleReportDate;
+    }
+
+    public Vehicle(){}
+
+    public String getVehicleId() {
+        return vehicleReportId;
+    }
+
+    public void setVehicleId(String vehicleReportId) {
+        this.vehicleReportId = vehicleReportId;
+    }
+
+    public String getVehicleRegNo() {
+        return vehicleRegNo;
+    }
+
+    public void setVehicleRegNo(String vehicleRegNo) {
+        this.vehicleRegNo = vehicleRegNo;
+    }
+
+    public String getVehicleImageUrl() {
+        return vehicleImageUrl;
+    }
+
+    public void setVehicleImageUrl(String vehicleImageUrl) {
+        this.vehicleImageUrl = vehicleImageUrl;
+    }
+
+    public String getVehicleReportDate() {
+        return vehicleReportDate;
+    }
+
+    public void setVehicleReportDate(String vehicleReportDate) {
+        this.vehicleReportDate = vehicleReportDate;
+    }
+}
