868ae3c6cd9f00fb3dc56ad2f7f39dda3f97f50c
JithinJude <jithin.jude68@gmail.com>
2020-08-02 07:40:47 +0530

    OCR back

--- app/src/main/java/com/sih/hawkeye/ocr/OcrActivity.java
+++ app/src/main/java/com/sih/hawkeye/ocr/OcrActivity.java
@@ -1,17 +1,37 @@
 package com.sih.hawkeye.ocr;
 
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.sih.hawkeye.R;
 
+import java.util.Objects;
+
 public class OcrActivity extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ocr);
 
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
         setTitle(getString(R.string.ocr_activity_title));
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+        }
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
     }
 }
